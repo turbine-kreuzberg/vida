900c37bbcb8f03aaeb7754953613f3e1ea8c1958
Ricardo Malveiro <ricardo.malveiro@turbinekreuzberg.com>
2022-08-24 11:49:40 +0100

    Add PurgeCSS to removed unused CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import purgecss from 'rollup-plugin-purgecss';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    purgecss({
+      content: [`./src/**/*.vue`],
+      variables: false,
+      safelist: {
+        standard: [
+          /(autv|lnil|lnir|fas?)/,
+          /-(leave|enter|appear)(|-(to|from|active))$/,
+          /^(?!(|.*?:)cursor-move).+-move$/,
+          /^router-link(|-exact)-active$/,
+          /data-v-.*/
+        ]
+      },
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+      }
+    })
+  ],
   build: {
     cssCodeSplit: false,
     rollupOptions: {
