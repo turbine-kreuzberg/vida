6e640138fa1f2fd7ddc576953c046b011cec027d
Rozbeh Chiryai Sharahi <rozbeh.sharahi@turbinekreuzberg.com>
2022-08-25 14:49:52 +0100

    Add scroll bar for module area (default 90vh)

---
+++ src/components/Molecule/ScrollArea.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  maxHeight?: string;
+}>();
+
+const maxHeights = props.maxHeight || '90vh';
+</script>
+<template>
+  <div class="melb-scroll-area" :style="{ maxHeight: maxHeight }">
+    <slot />
+  </div>
+</template>
+<style lang="scss">
+.melb-scroll-area {
+  max-height: 400px;
+  overflow: auto;
+}
+</style>
