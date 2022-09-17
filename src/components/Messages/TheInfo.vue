f741b9096906a1fe368603bfca1b02fc705ac262
Rozbeh Chiryai Sharahi <rozbeh.sharahi@turbinekreuzberg.com>
2022-09-17 18:09:31 +0100

    Show public safe places via osm
    It needs some learning and refactoring yet :)

---
+++ src/components/Messages/TheInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="the-info">
+    <slot />
+  </div>
+</template>
+<style lang="scss" scoped>
+.the-info {
+  border-left: 3px solid rgba(140, 186, 248, 0.35);
+  margin-bottom: 20px;
+  padding: 0 0 0 10px;
+  color: #666666;
+}
+</style>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -13,3 +13,31 @@ declare module '@vue-leaflet/vue-leaflet' {
   export const LMap: any;
   export const LTileLayer: any;
 }
+
+declare module 'leaflet-overpass-layer/src/OverPassLayer' {
+  import { Icon, Layer } from 'leaflet';
+
+  export interface OverPassLayerResult {
+    elements: {
+      [key: string]: string | number;
+      lat: number;
+      lon: number;
+      tags: {
+        [key: string]: string;
+      };
+    }[];
+  }
+
+  export interface OverPassLayerOptions {
+    query?: string;
+    minZoom?: int;
+    beforeRequest?: () => void;
+    afterRequest?: () => void;
+    onSuccess?: (data: OverPassLayerResult) => void;
+    markerIcon?: Icon;
+  }
+
+  export default class OverPassLayer extends Layer {
+    constructor(options: OverPassLayerOptions);
+  }
+}
