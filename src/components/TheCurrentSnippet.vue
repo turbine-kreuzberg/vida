d9258576a8a2cc7647bafa6d59733743b068aa7b
rozbehsharahi <rozbeh.sharahi@turbinekreuzberg.com>
2022-11-19 01:15:39 +0000

    [FEATURE] Copy to clipboard

--- src/components/TheCurrentSnippet.vue
+++ src/components/TheCurrentSnippet.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import snippet from './../plain/snippet-code.txt?raw';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useWidget } from '../composables/useWidget';
 
 const { configuration } = useWidget();
+
+const copySuccess = ref(false);
 
 const snippetPrepared = computed(() => {
   let snippedPrepared = snippet;
@@ -20,18 +22,79 @@ const snippetPrepared = computed(() => {
 
   return snippedPrepared;
 });
+
+const copyToClipboard = async () => {
+  const code = document.querySelector('.the-current-snippet pre')?.innerHTML;
+
+  function htmlDecode(input: string): string {
+    const doc = new DOMParser().parseFromString(input, 'text/html');
+    return doc.documentElement.textContent || '';
+  }
+
+  if (!code) {
+    throw new Error('Could not grasp code...');
+  }
+
+  await navigator.clipboard.writeText(htmlDecode(code));
+  copySuccess.value = true;
+
+  window.setTimeout(() => (copySuccess.value = false), 1000);
+};
+
+const style = computed(() => {
+  return {
+    '--color': configuration.value.getHandColor()
+  };
+});
 </script>
 <template>
-  <div class="the-code-content">
-    <pre v-text="snippetPrepared" />
+  <a :style="style" class="copy" @click="copyToClipboard">
+    &#x2398; Copy code to clipboard.
+    <span v-if="copySuccess">(done)</span>
+  </a>
+  <div class="the-current-snippet">
+    <div class="scroll-area">
+      <pre v-text="snippetPrepared" />
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
-.the-code-content {
-  padding: 1.5em;
+.the-current-snippet {
+  padding: 1em;
   background: #043454;
   color: #fff;
-  overflow-x: auto;
-  font-size: 1.2em;
+  height: 200px;
+  overflow: auto;
+  font-size: 0.8em;
+
+  pre {
+    margin: 0;
+  }
+
+  &::-webkit-scrollbar {
+    width: 0.5em;
+    height: 0.5em;
+  }
+
+  &::-webkit-scrollbar-track {
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #124973;
+  }
+}
+
+.copy {
+  display: inline-block;
+  cursor: pointer;
+  margin-bottom: 1em;
+  padding: 0.7em 1em;
+  font-size: 0.8em;
+  background: var(--color);
+  color: #000;
+
+  &:hover {
+    opacity: 0.8;
+  }
 }
 </style>
