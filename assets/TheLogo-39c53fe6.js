6ee6900def3354891d6a0e07c40a69874eb74aa7
RozbehSharahi <rozbeh.sharahi@gmail.com>
2023-03-01 22:42:41 +0000

    Deploying to live from @ turbine-kreuzberg/vida@36695a271837fb4e8b4b180e7497f6d663d3a254 🚀

--- assets/TheLogo-39c53fe6.js
+++
@@ -1,1 +0,0 @@
-import{k as c,o as t,e as _,p as a,i as d,j as o}from"../index.js";const n={},s=e=>(a("data-v-5d0c95a9"),e=e(),d(),e),i={class:"the-logo"},p=s(()=>o("div",{class:"first-line"},"vida",-1)),r=s(()=>o("div",{class:"second-line"},"widget",-1)),l=[p,r];function h(e,v){return t(),_("div",i,l)}const u=c(n,[["render",h],["__scopeId","data-v-5d0c95a9"]]);export{u as default};
