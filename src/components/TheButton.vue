7fed85835f9a3032f7f56ef8b09cd8ea9eb141b5
rozbehsharahi <rozbeh.sharahi@turbinekreuzberg.com>
2023-03-07 22:25:19 +0000

    [TASK] Try to make mailjet a bit more beautiful integrated

--- src/components/TheButton.vue
+++ src/components/TheButton.vue
@@ -4,12 +4,16 @@ import { computed } from 'vue';
 const props = defineProps<{
   color?: string;
   label?: string;
+  disabled?: boolean;
 }>();
 
 const color = computed(() => props.color || 'var(--violet)');
+const disabled = computed(() => !!props.disabled);
 </script>
 <template>
-  <a class="the-button" :style="{ '--color': color }"> <slot /> {{ label }} </a>
+  <a class="the-button" :class="{ disabled }" :style="{ '--color': color }">
+    <slot /> {{ label }}
+  </a>
 </template>
 <style lang="scss" scoped>
 a {
@@ -19,5 +23,10 @@ a {
   padding: 0.5em 2em;
   border-radius: 2em;
   color: #fff;
+
+  &.disabled {
+    pointer-events: none;
+    opacity: 0.5;
+  }
 }
 </style>
--- src/components/TheNewsletterSubscription.vue
+++ src/components/TheNewsletterSubscription.vue
@@ -1,21 +1,45 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue';
+import TheButton from './TheButton.vue';
+import { computed, ref } from 'vue';
 
-onMounted(() => {
+const iframeReference = ref<HTMLElement | null>(null);
+const isActive = ref(false);
+const isLoading = ref(false);
+
+const iframe = computed((): HTMLElement => {
+  if (!iframeReference.value) throw new Error('Iframe not found');
+  return iframeReference.value;
+});
+
+const activate = () => {
+  isActive.value = true;
+  isLoading.value = true;
+
   if (document.head.querySelector('#the-newsletter-subscription-script')) {
     return;
   }
 
-  const element = document.createElement('script');
-  element.setAttribute('id', 'the-newsletter-subscription-script');
-  element.setAttribute('src', 'https://app.mailjet.com/pas-nc-embedded-v1.js');
-  document.head.appendChild(element);
-});
+  const script = document.createElement('script');
+  script.setAttribute('id', 'the-newsletter-subscription-script');
+  script.setAttribute('src', 'https://app.mailjet.com/pas-nc-embedded-v1.js');
+  document.head.appendChild(script);
+
+  script.addEventListener('load', async () => {
+    await new Promise((r) => window.setTimeout(r, 500));
+    isLoading.value = false;
+    window.scrollTo({ top: iframe.value.offsetTop, behavior: 'smooth' });
+  });
+};
 </script>
 <template>
   <div class="the-newsletter-subscription">
+    <the-button v-if="!isActive" :color="'var(--dark-rosa)'" @click="activate">
+      Keep me up to date
+    </the-button>
+    <the-button v-if="isLoading" :disabled="true">Loading ...</the-button>
     <!--suppress HtmlDeprecatedAttribute -->
     <iframe
+      ref="iframeReference"
       data-w-type="embedded"
       frameborder="0"
       scrolling="no"
@@ -23,7 +47,19 @@ onMounted(() => {
       marginwidth="0"
       src="https://0kpw4.mjt.lu/wgt/0kpw4/m6p/form?c=b5cc1666"
       width="100%"
-      height="700"
+      :class="{ 'the-hidden': !isActive, 'the-invisible': isLoading }"
     ></iframe>
   </div>
 </template>
+<style lang="scss" scoped>
+iframe {
+  border: 2px solid var(--red);
+  border-radius: 2em;
+}
+.the-hidden {
+  display: none;
+}
+.the-invisible {
+  opacity: 0;
+}
+</style>
